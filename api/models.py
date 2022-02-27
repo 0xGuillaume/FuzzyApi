5ab3664d15a41cfd21f72659f97a1ae709fcb465
0xGuillaume <guillaumegasmi@protonmail.com>
2022-02-27 20:10:07 +0100

    Added database basemodel

---
+++ api/models.py
@@ -0,0 +1,31 @@
+from pydantic import BaseModel, Field
+#from bson import ObjectId
+#from typing import Optional
+
+
+class FooModel(BaseModel):
+    title: str
+    description: str
+
+
+"""
+class PyObjectId(ObjectId):
+
+    @classmethod
+    def __get_validators__(cls):
+        yield cls.validate
+
+    @classmethod
+    def validate(cls, v):
+        if not ObjectId.is_valid(v):
+            raise ValueError('Invalid objectid')
+        return ObjectId(v)
+
+    @classmethod
+    def __modify_schema__(cls, field_schema):
+        field_schema.update(type='string')
+"""
+
+
+
+
